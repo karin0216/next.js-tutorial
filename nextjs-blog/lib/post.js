6fe09c6751c1941561e76c6d456aff5f53cd7167
karin0216 <cheer.karin0216@gmail.com>
2022-02-02 11:27:55 +0900

    working on dynamic routes

--- nextjs-blog/lib/post.js
+++ nextjs-blog/lib/post.js
@@ -1,9 +1,41 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
+import { remark } from "remark";
+import html from "remark-html";
 
 // specify the directory we'd like to retrieve data
 const postsDirectory = path.join(process.cwd(), "posts");
+
+export function getAllPostIds() {
+  const fileNames = fs.readdirSync(postsDirectory);
+  return fileNames.map((fileName) => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, ""),
+      },
+    };
+  });
+}
+
+export async function getPostData(id) {
+  const fullPath = path.join(postsDirectory, `${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+
+  const matterResult = matter(fileContents);
+
+  // use remark to convert md into html sting
+  const processedContent = await remark()
+    .use(html)
+    .process(matterResult.content);
+  const contentHtml = processedContent.toString();
+
+  return {
+    id,
+    contentHtml,
+    ...matterResult.data,
+  };
+}
 
 export function getSortedPostData() {
   // get file names under /posts
